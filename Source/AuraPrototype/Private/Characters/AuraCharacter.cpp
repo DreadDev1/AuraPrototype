c43e9771dadc9ab69e818dea862b2fd48d948d27
DreadDev1 <kameen.avery@gmail.com>
2025-07-10 14:29:57 -0400

    Replace Base Character with AuraCharacter

--- Source/AuraPrototype/Private/Characters/AuraCharacter.cpp
+++ Source/AuraPrototype/Private/Characters/AuraCharacter.cpp
@@ -3,30 +3,20 @@
 
 #include "AuraPrototype/Public/Characters/AuraCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
-// Sets default values
+
 AAuraCharacter::AAuraCharacter()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-}
 
-// Called when the game starts or when spawned
-void AAuraCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
 
-// Called every frame
-void AAuraCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 }
-
-// Called to bind functionality to input
-void AAuraCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-}
-
--- Source/AuraPrototype/Public/Characters/AuraCharacter.h
+++ Source/AuraPrototype/Public/Characters/AuraCharacter.h
@@ -15,14 +15,10 @@ public:
 	// Sets default values for this character's properties
 	AAuraCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
 };
